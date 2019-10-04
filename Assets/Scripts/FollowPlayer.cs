76ba7e666f196e0c6c8f1d54f0534db8ceeb6a98
Douglas Urner <dlu@canishe.com>
2019-10-03 23:26:09 -0700

    Lesson 1.3 changes.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+	public GameObject player;
+	private Vector3 offset = new Vector3(0, 5, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Save the initial distance between the player's vehicle and the camera.
+		//offset = player.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Maintain the camera's position relative to the player.
+		transform.position = player.transform.position + offset;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	public float speed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle (player) forward.
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
