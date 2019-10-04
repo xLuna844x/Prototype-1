25bde6b4f9bb7193c1e55170580e903237430845
Douglas Urner <dlu@canishe.com>
2019-10-03 23:54:41 -0700

    Complete prototype

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	public float speed = 20.0f;
+	public float turnSpeed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		// Get player input.
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float forwardInput = Input.GetAxis("Vertical");
+
         // Move the vehicle (player) forward.
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+		// Turn the vehicle.
+		transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
