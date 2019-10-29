0ae3b29ffe4ce900654578d11c69a02ebf848823
Riley Ruiz <56165453+xLuna844x@users.noreply.github.com>
2019-10-29 08:23:08 -0700

    Fixed script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+
     }
 }
